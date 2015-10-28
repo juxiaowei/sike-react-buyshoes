87db85f596604b77d2cefa8ad5b91f077966493b
Jugg <505490@qq.com>
2015-10-28 21:27:19 +0800

    update

--- js/app.js
+++ js/app.js
@@ -8,3 +8,8 @@ function makeCartScrollNicely() {
   var cart = document.querySelector(".cart");
   Ps.initialize(cart);
 }
+
+var $toggle = document.querySelector(".site__right-sidebar-toggle");
+$toggle.addEventListener("click",function() {
+  document.body.classList.toggle("js-show-right-sidebar");
+});
